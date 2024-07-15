65ac5123ee26c23c28d46bf40a9c83d40b410cb0
SamuelHaleDev <samuelhalebusiness@gmail.com>
2024-07-15 09:40:44 -0400

    Implemented testInsertAllFixes

--- Program.cs
+++ Program.cs
@@ -397,7 +397,57 @@ class Program
 
     public static bool testInsertAllFixes()
     {
-        return false;
+        TestableRedBlackTree rbtree = new TestableRedBlackTree();
+
+        rbtree.insert(10);
+        rbtree.insert(5);
+        rbtree.insert(7);
+
+        /*
+        LLRB Tree Representation:
+            (7)
+            ├── (5)
+            └── (10)
+
+         */
+
+        // root
+        if (rbtree.root is null) return false;
+        if (!rbtree.root.isBlack) return false;
+        if (rbtree.root.item != 7) return false;
+
+        // left
+        if (rbtree.root.left is null) return false;
+        if (rbtree.root.left.isBlack) return false;
+        if (rbtree.root.left.item != 5) return false;
+
+        // left.left
+        if (rbtree.root.left.left is not null) return false;
+
+        // left.right
+        if (rbtree.root.left.right is not null) return false;
+
+        // right
+        if (rbtree.root.right is null) return false;
+        if (rbtree.root.right.isBlack) return false;
+        if (rbtree.root.right.item != 10) return false;
+
+        // right.left
+        if (rbtree.root.right.left is not null) return false;
+
+        // right.right
+        if (rbtree.root.right.right is not null) return false;
+
+        Console.WriteLine("Number of Calls to Flip Colors after inserting (10, 7, 5) in order");
+        if (rbtree.callsToFlipColors != 1) return false;
+
+        Console.WriteLine("Number of Calls to Rotate Left after inserting (10, 7, 5) in order");
+        if (rbtree.callsToRotateLeft != 1) return false;
+
+        Console.WriteLine("Number of Calls to Rotate Right after inserting (10, 7, 5) in order");
+        if (rbtree.callsToRotateRight != 1) return false;
+
+        return true;
     }
 
     public static bool testInsertUpwardPropagation()
